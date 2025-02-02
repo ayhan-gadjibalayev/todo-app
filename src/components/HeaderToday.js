5efffe9fffe8f84e156505d3c477e90a6d11c404
Ayhan <ayhan.gadjibalayev@gmail.com>
2025-02-02 12:35:36 +0300

    time

--- src/components/HeaderToday.js
+++ src/components/HeaderToday.js
@@ -1,23 +1,50 @@
 import React from "react";
+import { IoTrashBin } from "react-icons/io5";
 
-export default function HeaderToday (props) {
-    const {username} = props;
+export default function HeaderToday(props) {
+  const { username, buttondelete } = props;
 
-    return (
-        <div className="header-container">
-        <div className="right-header-block">
-          <span style={{ fontSize: "30px" }}>
-            <b>Доброе утро, {username}! 👋</b>
-          </span>
-          <span style={{ fontSize: "25px", color: "grey" }}>Сегодня среда, 26 июля 2023 г.</span>
-        </div>
-        <div className="left-header-block">
-          <div className="strelka-block">
-            <span className="down-arrow">⮟</span>
-            <span style={{ font: "20px", marginLeft: "10px" }}>Сегодня</span>
-          </div>
-          <span className="equals">=</span>
-        </div>
+  const today = new Date();
+
+  const formattedDate = today
+    .toLocaleDateString("ru-RU", {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    })
+    .replace(/\./g, "")
+    .trim();
+
+  const daysOfWeek = [
+    "Воскресенье",
+    "Понедельник",
+    "Вторник",
+    "Среда",
+    "Четверг",
+    "Пятница",
+    "Суббота",
+  ];
+
+  const dayIndex = today.getDay();
+
+  const currentDay = daysOfWeek[dayIndex];
+
+  return (
+    <div className="header-container">
+      <div className="right-header-block">
+        <span style={{ fontSize: "30px" }}>
+          <b>Доброе утро, {username}! 👋</b>
+        </span>
+        <span style={{ fontSize: "25px", color: "grey" }}>
+          Сегодня {currentDay}, {formattedDate}</span>
       </div>
-    )
+      <div className="left-header-block">
+        <div className="strelka-block">
+          <span className="down-arrow">⮟</span>
+          <span style={{ fontSize: "20px", marginLeft: "10px" }}>Сегодня</span>
+        </div>
+        <IoTrashBin className="equals" onClick={buttondelete} />
+      </div>
+    </div>
+  );
 }
